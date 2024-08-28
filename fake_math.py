5bc77602f9ce442604aff93dcc15c4609f8d01c9
Facerevers <128045506+Facerevers@users.noreply.github.com>
2024-08-28 15:13:49 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide_fake(first, second):
+    result=0
+    txt = "Ошибка!"
+    if second == 0:
+        return txt
+    else:
+        result= first/second
+        return result
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide_true(first, second):
+    result=0
+    if second == 0:
+        return inf
+    else:
+        result= first/second
+        return result
