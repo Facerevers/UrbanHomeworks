519c2ab626e7c1a48b216dd6f89ed0c19ce13cd0
Facerevers <128045506+Facerevers@users.noreply.github.com>
2024-09-29 15:33:04 +0300

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,50 @@
+import os.path
+
+
+class Product:
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        result = f"{self.name}, {self.weight}, {self.category}"
+        return result
+
+
+class Shop:
+
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        if os.path.exists(self.__file_name) == False:
+            file = open(self.__file_name, 'w+')
+        else:
+            file = open(self.__file_name, 'r')
+        result = file.read()
+        file.close()
+        return result
+
+    def add(self, *products):
+        existing_products = self.get_products()
+        for p in products:
+            if p.name in existing_products:
+                print(f"Продукт {p.name} уже есть в магазине")
+            else:
+                file = open(self.__file_name, 'a')
+                file.write(f"{p.name}, {p.weight}, {p.category}\n")
+                file.close()
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
