b8ef31ee23558326f633a1a880bb29dc10a3b39b
Facerevers <128045506+Facerevers@users.noreply.github.com>
2024-12-02 20:47:24 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -17,19 +17,10 @@ async def start(message):
     print("Привет! Я бот помогающий твоему здоровью.")
 
 
-@dp.message_handler(commands=["start"])
-async def start_message(message):
-    print("Start message")
-
-
 @dp.message_handler()
 async def all_messages(message):
     print("Введите команду /start, чтобы начать общение.")
 
-
-@dp.message_handler()
-async def all_message(message):
-    print("Мы получили сообщение!")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
