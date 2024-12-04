96563216d48394573b252a65cb12affd8969922c
Facerevers <128045506+Facerevers@users.noreply.github.com>
2024-12-05 02:11:10 +0300

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
 
-api = "7740794913:AAFiIoOpc9rnb2Wcf3KvDXkYY5QE6ylhEUY"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -27,23 +27,23 @@ async def urban_message(message):
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
-    await state.update.data(first=message.text)
+    await state.update_data(age=message.text)
     await message.answer("Введите свой рост:")
     await UserState.growth.set()
 
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
-    await state.update.data(first=message.text)
+    await state.update_data(growth=message.text)
     await message.answer("Введите свой вес:")
     await UserState.weight.set()
 
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
-    await state.update.data(first=message.text)
+    await state.update_data(weight=message.text)
     data = await state.get_data()
-    calories = 10 * data.weight + 6.25 * data.growth - 5 * data.age - 5
+    calories = 10 * float(data["weight"]) + 6.25 * float(data["growth"]) - 5 * float(data["age"]) - 5
     await message.answer(f"Ваша норма калорий: {calories}")
     await state.finish()
 
