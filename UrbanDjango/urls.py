b3ac937a672d9e9bb39b4a6f07d50f06a5c94257
Lkrovinskii <lkrovin@mail.ru>
2025-01-21 14:46:30 +0300

    Create Url's

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from UrbanDjango.task2.views import index, index2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('func/', index),
+    path('class/', index2.as_view),
 ]
